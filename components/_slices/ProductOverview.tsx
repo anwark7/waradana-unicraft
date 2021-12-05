969a419f6e867e51d866dbc7a1b73ef3db1052d1
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-05 20:35:21 +0700

    Update productOverview

--- components/_slices/ProductOverview.tsx
+++ components/_slices/ProductOverview.tsx
@@ -12,13 +12,13 @@ const ProductOverview = ({slice}: Props): JSX.Element => {
 	
 	return (
 		<section className="flex-cc col bg-secondary w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
-			<p className="font-bold text-secondary text-center text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl">{slice.primary.title}</p>
-			<div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+			<h1 className="mb-16 font-bold text-primary text-center text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl">{slice.primary.title}</h1>
+			<div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 				{slice.items.map((item, index) => (
 					<Link href={`/product#${item.content.uid}`} 
 						style={{backgroundImage: `url(${item.content.data.image.url})`}}
 						className="flex-cc w-full relative pb-[100%] bg-cover bg-center" key={index}>
-						<p className="absolute top-[45%] font-bold text-secondary md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+						<p className="absolute top-[45%] font-bold text-secondary text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
 							{item.content.data.title}
 						</p>
 					</Link>
@@ -26,7 +26,7 @@ const ProductOverview = ({slice}: Props): JSX.Element => {
 			</div>
 			<Link href={'/product'}
 				style={{backgroundImage: `url(${slice.primary.image.url})`}}
-				className="flex-cc w-full h-20 mt-10 bg-cover bg-center font-bold text-secondary text-lg sm:text-2xl lg:text-3xl">
+				className="flex-cc w-full h-24 mt-6 bg-cover bg-center font-bold text-secondary text-lg sm:text-2xl lg:text-4xl">
 				{slice.primary.button_text}
 			</Link>
 		</section>
