1802cd56f87882813af14c1ab69ddc8135b14b99
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-05 20:34:06 +0700

    Update link ContactUs

--- components/_slices/ContactUs.tsx
+++ components/_slices/ContactUs.tsx
@@ -15,16 +15,16 @@ const ContactUs = ({slice}: Props): JSX.Element => {
 	
 	return (
 		<section className="flex-cc col bg-background w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
-			<h1 className="w-full text-center font-bold text-primary text-2xl md:text-5xl xl:text-6xl">{slice.primary.title}</h1>
+			<h1 className="w-full text-center font-bold text-primary text-3xl md:text-5xl xl:text-6xl">{slice.primary.title}</h1>
 			<div className="flex-vs -sm:col w-full my-12 md:my-20">
-				<div className="flex-cc sm:col gap-4">
+				<Link href={`mailto:${slice.primary.email}`} className="flex-cc sm:col gap-4">
 					<MdEmail className="text-primary" size={70}/>
 					<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.email}</p>
-				</div>
-				<div className="flex-cc sm:col gap-4">
+				</Link>
+				<Link href={`https://api.whatsapp.com/send?phone=62${(slice.primary.phone).substring(1)}`} className="flex-cc sm:col gap-4">
 					<FaWhatsappSquare className="text-primary" size={70}/>
 					<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.phone}</p>
-				</div>
+				</Link>
 				<div className="flex-cc sm:col gap-4">
 					<FaMapMarkedAlt className="text-primary" size={70}/>
 					<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.address}</p>
