77cc3179e48cbb4de44c21bff250bea78fe7bf54
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-05 20:35:29 +0700

    Update Hero

--- components/_slices/Hero/Hero.tsx
+++ components/_slices/Hero/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = ({slice}: Props): JSX.Element => {
 	return (
 		<section
 			style={{backgroundImage: `url(${slice.primary.image.url})`}}
-			className="flex-cc md:flex-se w-full md:py-24 md:px-32 lg:py-28 lg:px-36 xl:py-32 xl:px-40 h-[280px] sm:h-[320px] md:h-[420px] xl:h-[520px] bg-cover bg-center">
+			className="flex-cc md:flex-se w-full md:py-24 md:px-32 lg:py-28 lg:px-36 xl:py-32 xl:px-40 h-[380px] sm:h-[420px] md:h-[520px] xl:h-[620px] bg-cover bg-center">
 			<div className="flex-cc md:flex-cs md:w-1/2 col">
 				<p className="font-bold !text-secondary md:text-xl xl:text-4xl 2xl:text-5xl">
 					{slice.primary.title}
--- components/_slices/Hero/HomeHero.tsx
+++ components/_slices/Hero/HomeHero.tsx
@@ -12,12 +12,12 @@ const HomeHero = ({slice}: Props): JSX.Element => {
 	return (
 		<section
 			style={{backgroundImage: `url(${slice.primary.image.url})`}}
-			className="flex-cc md:flex-se w-full md:py-24 md:px-32 lg:py-28 lg:px-36 xl:py-32 xl:px-40 h-[280px] sm:h-[320px] md:h-[420px] xl:h-[520px] bg-cover bg-center">
-			<div className="flex-cc md:flex-cs md:w-1/2 col">
-				<p className="font-bold text-primary text-md md:text-xl xl:text-4xl 2xl:text-5xl">
+			className="flex-cc md:flex-se w-full md:py-24 md:px-32 lg:py-28 lg:px-36 xl:py-32 xl:px-40 h-[380px] sm:h-[420px] md:h-[520px] xl:h-[620px] bg-cover bg-center">
+			<div className="flex-cc md:flex-cs md:w-2/3 lg:w-1/2 col">
+				<p className="font-bold text-primary -md:text-center -md:px-16 text-xl md:text-2xl xl:text-5xl 2xl:text-6xl">
 					{slice.primary.heading}
 				</p>
-				<p className="font-bold !text-secondary md:lg xl:text-3xl 2xl:text-4xl">
+				<p className="font-bold !text-secondary -md:text-center -md:px-10 md:text-lg xl:text-3xl 2xl:text-4xl">
 					{slice.primary.description}
 				</p>
 			</div>
