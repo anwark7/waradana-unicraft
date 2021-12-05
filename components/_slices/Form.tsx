bd032fe3100aceaa1785b2f9b53d5c16b6b53255
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-05 20:34:53 +0700

    update form

--- components/_slices/Form.tsx
+++ components/_slices/Form.tsx
@@ -4,6 +4,7 @@ import useDebug from '@core/hooks/useDebug';
 import { MdEmail } from 'react-icons/md';
 import { FaWhatsappSquare, FaMapMarkedAlt } from 'react-icons/fa';
 import useForm from '@core/hooks/useForm';
+import Link from '@components/_shared/Link';
 
 
 interface Props {
@@ -20,17 +21,17 @@ const Form = ({slice}: Props): JSX.Element => {
 	
 	return (
 		<section className="flex-cc col gap-16 bg-secondary w-full h-[1000px] py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
-			<h1 className="font-bold text-primary text-2xl md:text-5xl xl:text-6xl">{slice.primary.title}</h1>
+			<h1 className="font-bold text-primary text-3xl md:text-5xl xl:text-6xl">{slice.primary.title}</h1>
 			<div className="flex-cc -md:col w-full gap-8">
 				<div className="flex-vs md:col w-full md:gap-8 md:w-1/3 my-12 md:my-20">
-					<div className="flex-cc -lg:col gap-4">
+					<Link href={`mailto:${slice.primary.email}`}  className="flex-cc -lg:col gap-4">
 						<MdEmail className="text-primary" size={70}/>
 						<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.email}</p>
-					</div>
-					<div className="flex-cc -lg:col gap-4">
+					</Link>
+					<Link href={`https://api.whatsapp.com/send?phone=62${(slice.primary.phone).substring(1)}`} className="flex-cc -lg:col gap-4">
 						<FaWhatsappSquare className="text-primary" size={70}/>
 						<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.phone}</p>
-					</div>
+					</Link>
 					<div className="flex-cc -lg:col gap-4">
 						<FaMapMarkedAlt className="text-primary" size={70}/>
 						<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.address}</p>
