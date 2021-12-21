59e1752c57f7f891e07c0dfea6d1ba535a39b39e
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:30:58 +0700

    update navbar

--- components/_slices/layout/Navbar.tsx
+++ components/_slices/layout/Navbar.tsx
@@ -29,15 +29,15 @@ const Navbar = ({slice}: Props): JSX.Element => {
 			<section
 				className={`flex-bc 
 			${onScroll ? 'bg-primary border-b-4 border-[#B4846C] text-secondary' : 'bg-transparent text-white'} 
-			px-8 py-4 md:px-12 md:py-8 xl:px-16`}>
-				<div className="w-16 md:w-24 h-16 md:h-24">
-					<img src={slice.primary.logo.url} alt={slice.primary.logo.alt} />
-				</div>
+			py-4 md:py-5 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32`}>
+				<Link href={'/'} className="h-16 md:h-20">
+					<img className="h-full" src={slice.primary.logo.url} alt={slice.primary.logo.alt} />
+				</Link>
 				<div className="-md:hidden flex-cc gap-12">
-					<Link href={'/'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg xl:text-2xl">Home</Link>
-					<Link href={'/about'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg xl:text-2xl">About</Link>
-					<Link href={'/product'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg xl:text-2xl">Product</Link>
-					<Link href={'/contact'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg xl:text-2xl">Contact</Link>
+					<Link href={'/'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg 2xl:text-xl">Home</Link>
+					<Link href={'/about'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg 2xl:text-xl">About</Link>
+					<Link href={'/product'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg 2xl:text-xl">Product</Link>
+					<Link href={'/contact'} className="hover:text-[#B4846C] hover:cursor-pointer font-bold text-lg 2xl:text-xl">Contact</Link>
 				</div>
 				<div onClick={() => {setOpenDropdown(!openDropdown);}} className="md:hidden">
 					<GiHamburgerMenu className={`${onScroll ? 'text-secondary hover:cursor-pointer' : 'text-primary hover:cursor-pointer'}`} size={40}/>
