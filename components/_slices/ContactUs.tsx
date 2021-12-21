723198c2eaafaa03cd4d20a5e023ef51b4fc4434
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:29:03 +0700

    update contact us

--- components/_slices/ContactUs.tsx
+++ components/_slices/ContactUs.tsx
@@ -18,19 +18,19 @@ const ContactUs = ({slice}: Props): JSX.Element => {
 			<h1 className="w-full text-center font-bold text-primary text-3xl md:text-5xl xl:text-6xl">{slice.primary.title}</h1>
 			<div className="flex-vs -sm:col w-full my-12 md:my-20">
 				<Link href={`mailto:${slice.primary.email}`} className="flex-cc sm:col gap-4">
-					<MdEmail className="text-primary" size={70}/>
-					<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.email}</p>
+					<MdEmail className="text-primary text-[40px] md:text-[60px]"/>
+					<p className="text-primary-lighter md:text-2xl w-56 text-left md:text-center">{slice.primary.email}</p>
 				</Link>
 				<Link href={`https://api.whatsapp.com/send?phone=62${(slice.primary.phone).substring(1)}`} className="flex-cc sm:col gap-4">
-					<FaWhatsappSquare className="text-primary" size={70}/>
-					<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.phone}</p>
+					<FaWhatsappSquare className="text-primary text-[40px] md:text-[60px]"/>
+					<p className="text-primary-lighter md:text-2xl w-56 text-left md:text-center">{slice.primary.phone}</p>
 				</Link>
 				<div className="flex-cc sm:col gap-4">
-					<FaMapMarkedAlt className="text-primary" size={70}/>
-					<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center break-words">{slice.primary.address}</p>
+					<FaMapMarkedAlt className="text-primary text-[40px] md:text-[60px]"/>
+					<p className="text-primary-lighter md:text-2xl w-56 text-left md:text-center break-words">{slice.primary.address}</p>
 				</div>
 			</div>
-			<Link href={'/contact'} className="flex-cc w-full h-20 mt-10 rounded-2xl bg-primary font-bold text-secondary text-lg sm:text-2xl lg:text-3xl">
+			<Link href={'/contact'} className="flex-cc w-full py-2 sm:py-5 px-6 mt-10 rounded-2xl bg-primary font-bold text-secondary text-lg sm:text-2xl lg:text-3xl">
 				{slice.primary.button_text}
 			</Link>
 		</section>
