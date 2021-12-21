888422e36bf4aa34c0e42f76e1975543da9329bf
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:30:09 +0700

    update oveview

--- components/_slices/ProductOverview.tsx
+++ components/_slices/ProductOverview.tsx
@@ -11,8 +11,8 @@ const ProductOverview = ({slice}: Props): JSX.Element => {
 	useDebug(slice);
 	
 	return (
-		<section className="flex-cc col bg-secondary w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
-			<h1 className="mb-16 font-bold text-primary text-center text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl">{slice.primary.title}</h1>
+		<section className="flex-cc col bg-secondary w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-6">
+			<h1 className="mb-12 md:mb-16 font-bold text-primary text-center text-2xl lg:text-4xl xl:text-6xl">{slice.primary.title}</h1>
 			<div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 				{slice.items.map((item, index) => (
 					<Link href={`/product#${item.content.uid}`} 
@@ -26,7 +26,7 @@ const ProductOverview = ({slice}: Props): JSX.Element => {
 			</div>
 			<Link href={'/product'}
 				style={{backgroundImage: `url(${slice.primary.image.url})`}}
-				className="flex-cc w-full h-24 mt-6 bg-cover bg-center font-bold text-secondary text-lg sm:text-2xl lg:text-4xl">
+				className="flex-cc w-full h-64 mt-6 bg-cover bg-center font-bold text-secondary text-lg sm:text-2xl lg:text-4xl">
 				{slice.primary.button_text}
 			</Link>
 		</section>
