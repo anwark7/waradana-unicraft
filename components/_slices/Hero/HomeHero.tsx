3178d8227bb7148da701c3df43415e3f2346ce60
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:30:36 +0700

    update home hero

--- components/_slices/Hero/HomeHero.tsx
+++ components/_slices/Hero/HomeHero.tsx
@@ -12,12 +12,12 @@ const HomeHero = ({slice}: Props): JSX.Element => {
 	return (
 		<section
 			style={{backgroundImage: `url(${slice.primary.image.url})`}}
-			className="flex-cc md:flex-se w-full md:py-24 md:px-32 lg:py-28 lg:px-36 xl:py-32 xl:px-40 h-[380px] sm:h-[420px] md:h-[520px] xl:h-[620px] bg-cover bg-center">
-			<div className="flex-cc md:flex-cs md:w-2/3 lg:w-1/2 col">
+			className="flex-ce md:flex-se w-full py-12 md:py-24 lg:py-28 xl:py-32 sm:px-13 md:px-24 lg:px-28 xl:px-32 h-[380px] sm:h-[420px] md:h-screen bg-cover bg-center">
+			<div className="flex-cc md:flex-cs gap-2 md:w-2/3 col">
 				<p className="font-bold text-primary -md:text-center -md:px-16 text-xl md:text-2xl xl:text-5xl 2xl:text-6xl">
 					{slice.primary.heading}
 				</p>
-				<p className="font-bold !text-secondary -md:text-center -md:px-10 md:text-lg xl:text-3xl 2xl:text-4xl">
+				<p className="lg:w-3/4 font-bold !text-secondary -md:text-center -md:px-10 md:text-lg xl:text-3xl 2xl:text-4xl">
 					{slice.primary.description}
 				</p>
 			</div>
