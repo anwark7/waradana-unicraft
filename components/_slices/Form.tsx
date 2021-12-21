5fadd982f8b6a15288c3ec048b77a4bfcff5e22f
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:29:14 +0700

    update form

--- components/_slices/Form.tsx
+++ components/_slices/Form.tsx
@@ -36,24 +36,24 @@ const Form = ({slice}: Props): JSX.Element => {
 	};
 	
 	return (
-		<section className="flex-cc col gap-16 bg-secondary w-full h-[1000px] py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
-			<h1 className="font-bold text-primary text-3xl md:text-5xl xl:text-6xl">{slice.primary.title}</h1>
+		<section className="flex-cc col gap-10 bg-gray-100 w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
+			<h1 className="font-bold text-primary text-5xl xl:text-6xl">{slice.primary.title}</h1>
 			<div className="flex-cc -md:col w-full gap-8">
 				<div className="flex-vs md:col w-full md:gap-8 md:w-1/3 my-12 md:my-20">
 					<Link href={`mailto:${slice.primary.email}`}  className="flex-cc -lg:col gap-4">
-						<MdEmail className="text-primary" size={70}/>
-						<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.email}</p>
+						<MdEmail className="text-primary text-[50px] md:text-[60px]"/>
+						<p className="-md:hidden text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.email}</p>
 					</Link>
 					<Link href={`https://api.whatsapp.com/send?phone=62${(slice.primary.phone).substring(1)}`} className="flex-cc -lg:col gap-4">
-						<FaWhatsappSquare className="text-primary" size={70}/>
-						<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.phone}</p>
+						<FaWhatsappSquare className="text-primary text-[50px] md:text-[60px]"/>
+						<p className="-md:hidden text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.phone}</p>
 					</Link>
 					<div className="flex-cc -lg:col gap-4">
-						<FaMapMarkedAlt className="text-primary" size={70}/>
-						<p className="text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.address}</p>
+						<FaMapMarkedAlt className="text-primary text-[50px] md:text-[60px]"/>
+						<p className="-md:hidden text-primary-lighter text-lg md:text-2xl w-56 text-center">{slice.primary.address}</p>
 					</div>
 				</div>
-				<div className="w-full h-2 md:w-2 md:h-full bg-primary"></div>
+				<div className="w-full h-2 md:w-2 md:h-[900px] bg-primary"></div>
 				<form onSubmit={handleSubmit} className="w-full flex-cc md:flex-cs col gap-8">
 					<div className="w-full flex-cs col">
 						<label className="mb-3 font-bold text-lg md:text-2xl lg:text-3xl text-primary" htmlFor="name">
