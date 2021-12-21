c1bc4fa280d2a8bd78c91a058d1f82e5cc4ff518
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:30:27 +0700

    update hero

--- components/_slices/Hero/Hero.tsx
+++ components/_slices/Hero/Hero.tsx
@@ -12,9 +12,9 @@ const Hero = ({slice}: Props): JSX.Element => {
 	return (
 		<section
 			style={{backgroundImage: `url(${slice.primary.image.url})`}}
-			className="flex-cc md:flex-se w-full md:py-24 md:px-32 lg:py-28 lg:px-36 xl:py-32 xl:px-40 h-[380px] sm:h-[420px] md:h-[520px] xl:h-[620px] bg-cover bg-center">
+			className="flex-ce md:flex-se w-full py-12 md:py-24 lg:py-28 xl:py-32 sm:px-13 md:px-24 lg:px-28 xl:px-32 h-[380px] sm:h-[420px] md:h-screen bg-cover bg-center">
 			<div className="flex-cc md:flex-cs md:w-1/2 col">
-				<p className="font-bold !text-secondary md:text-xl xl:text-4xl 2xl:text-5xl">
+				<p className="font-bold !text-secondary text-4xl xl:text-5xl 2xl:text-6xl">
 					{slice.primary.title}
 				</p>
 			</div>
