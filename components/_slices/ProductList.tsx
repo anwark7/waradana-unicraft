11b25984d740171faa7c2afcb43a4aa03520d5dc
anwark7 <anwarkhairuddin11@gmail.com>
2021-12-22 01:29:52 +0700

    update product list

--- components/_slices/ProductList.tsx
+++ components/_slices/ProductList.tsx
@@ -12,17 +12,17 @@ const ProductList = ({slice}: Props): JSX.Element => {
 	useDebug(slice);
 	
 	return (
-		<section className="flex-cc col gap-16 bg-secondary w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
+		<section className="flex-cc col gap-20 w-full py-10 md:py-16 xl:py-24 2xl:py-28 px-12 sm:px-13 md:px-24 lg:px-28 xl:px-32">
 			{slice.items.map((item, index) => (
 				<div className="flex-vs gap-6 -md:col w-full" key={index}>
 					{item.content.data.body.map((slice, index) => (
 						<RenderSlice slice={slice} key={index}/>
 					))}
-					<div className="flex-cc md:flex-bs gap-16 col w-full md:w-7/12">
+					<div className="flex-cc md:flex-bs gap-8 md:gap-16 col w-full md:w-7/12">
 						<p className="text-primary font-bold text-4xl sm:text-5xl">
 							{item.content.data.title}
 						</p>
-						<p className="!text-primary-lighter font-bold text-sm sm:text-base md:text-lg lg:text-2xl">
+						<p className="!text-primary-lighter font-bold text-center md:text-left text-sm sm:text-base md:text-lg lg:text-2xl">
 							{RichText.asText(item.content.data.description)}
 						</p>
 					</div>
