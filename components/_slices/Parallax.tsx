d89502abf5ba83e0151e004caeb11367345f9978
anwark7 <anwarkhairuddin11@gmail.com>
2022-01-07 16:23:31 +0700

    Update parallax

--- components/_slices/Parallax.tsx
+++ components/_slices/Parallax.tsx
@@ -11,7 +11,7 @@ const Parallax = ({slice}: Props): JSX.Element => {
 		<section 
 			style={{backgroundImage: `url(${slice.primary.image.url})`}}
 			className="flex-cc w-full h-52 sm:h-72 md:h-80 lg:h-[460px] bg-cover bg-center bg-fixed px-10 sm:px-28 md:px-36 lg:px-44 xl:px-72">
-			<p className="font-bold text-secondary text-center text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl">{slice.primary.text}</p>
+			<p className="font-bold text-white text-center text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl">{slice.primary.text}</p>
 		</section>
 	);
 };
